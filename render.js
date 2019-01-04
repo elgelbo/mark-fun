a53569a0c30f21f10da673fb2601e5ee05f06fb6
el_gelbo <mgelbman@gmail.com>
2019-01-04 11:14:45 -0800

    add generator for null template files

--- render.js
+++ render.js
@@ -13,18 +13,25 @@ fs.readdir(pages, (err, files) => {
     var str = fs.readFileSync(`./pages/${file}`, "utf8");
     // PARSE FRONT MATTER W/DATA AND CONTENT
     const theMatter = matter(str);
-    console.log(theMatter.data);
-    
+
     // GET NAME STRING OF CONTENT FILE
     const fileName = file.split('.')[0];
     // CONVERT MARKDOWN TO HTML
     const prettyDown = md.render(theMatter.content);
     // RENDER TEMPALTE WITH MD CONTENT
-    const pretty = nunjucks.render(`${fileName}.njk`, { content: prettyDown, data: theMatter.data });
-    // SAVE FILE WITH STRING NAME
-    fs.writeFile(`./src/${fileName}.html`, pretty, function (err) {
-      if (err) throw err;
-      console.log(`Saved: /src/${fileName}.html`);
+    // const pretty = nunjucks.render(`${fileName}.njk`, { content: prettyDown, data: theMatter.data });
+    const pretty = nunjucks.render(`${fileName}.njk`, { content: prettyDown, data: theMatter.data }, function (err, res) {
+      if (err) {
+        const pretty = nunjucks.render(`base.njk`, { layoutContent: prettyDown, data: theMatter.data });
+        fs.writeFile(`./src/${fileName}.html`, pretty, function (err) {
+          if (err) {throw err};
+          console.log(`Saved: /src/${fileName}.html`);
+        });
+      } else {
+        fs.writeFile(`./src/${fileName}.html`, res, function (err) {
+          if (err) {throw err};
+          console.log(`Saved: /src/${fileName}.html`);
+        });      }
     });
   });
 });
