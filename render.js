152f797f18d34b0fd6051d0392e76549557ba7c6
el_gelbo <mgelbman@gmail.com>
2019-01-04 13:32:27 -0800

    refactor render, generate nav

--- render.js
+++ render.js
@@ -1,37 +1,13 @@
 const fs = require('fs')
-const matter = require('gray-matter');
-const md = require('markdown-it')();
-const nunjucks = require('nunjucks')
-const pages = './pages/';
+const createGlobal = require('./utils/createGlobal')
 
-nunjucks.configure('views', { autoescape: false });
-// READ PAGES DIR
-fs.readdir(pages, (err, files) => {
-  // ITERATE OVER EACH FILE IN PAGES DIR
-  files.forEach(file => {
-    // READ MARKDOWN FILE (EDITABLE BY CMS/GIT)
-    var str = fs.readFileSync(`./pages/${file}`, "utf8");
-    // PARSE FRONT MATTER W/DATA AND CONTENT
-    const theMatter = matter(str);
+generate = async () => {
+  try {
+    const data = createGlobal.grey();
+    const nav = createGlobal.nav(data);
+    createGlobal.render(data, nav);
+  } catch (e) {
 
-    // GET NAME STRING OF CONTENT FILE
-    const fileName = file.split('.')[0];
-    // CONVERT MARKDOWN TO HTML
-    const prettyDown = md.render(theMatter.content);
-    // RENDER TEMPALTE WITH MD CONTENT
-    // const pretty = nunjucks.render(`${fileName}.njk`, { content: prettyDown, data: theMatter.data });
-    const pretty = nunjucks.render(`${fileName}.njk`, { content: prettyDown, data: theMatter.data }, function (err, res) {
-      if (err) {
-        const pretty = nunjucks.render(`base.njk`, { placeContent: prettyDown, data: theMatter.data });
-        fs.writeFile(`./src/${fileName}.html`, pretty, function (err) {
-          if (err) {throw err};
-          console.log(`Saved: /src/${fileName}.html`);
-        });
-      } else {
-        fs.writeFile(`./src/${fileName}.html`, res, function (err) {
-          if (err) {throw err};
-          console.log(`Saved: /src/${fileName}.html`);
-        });      }
-    });
-  });
-});
+  }
+};
+generate();
---
+++ utils/createGlobal.js
@@ -0,0 +1,49 @@
+const fs = require('fs')
+const matter = require('gray-matter');
+const md = require('markdown-it')();
+const nunjucks = require('nunjucks')
+const pages = './pages/';
+nunjucks.configure('views', { autoescape: false });
+
+nunjucks.configure('views', { autoescape: false });
+
+exports.grey = () => {
+  const rawMatter = new Array();
+  const pages = fs.readdirSync('./pages');
+  pages.forEach(page => {
+    var str = fs.readFileSync(`./pages/${page}`, "utf8");
+    const data = matter(str);
+    const fileName = page.split('.')[0];
+    data.slug = fileName;
+    rawMatter.push(data);
+  })
+  return rawMatter;
+};
+
+exports.nav = (data) => {
+  const navData = new Array();
+  data.forEach(item => {
+    navData.push(item.slug);
+  });
+  return navData;
+};
+
+exports.render = (data, nav) => {
+  data.forEach(item => {
+    const prettyDown = md.render(item.content);
+    nunjucks.render(`${item.slug}.njk`, { content: prettyDown, data: item.data, navData: nav }, function (err, res) {
+      if (!res) {
+        const pretty = nunjucks.render(`base.njk`, { placeContent: prettyDown, data: item.data, navData: nav });
+        fs.writeFile(`./src/${item.slug}.html`, pretty, function (err) {
+          if (err) { throw err };
+          console.log(`Saved: /src/${item.slug}.html`);
+        });
+      } else {
+        fs.writeFile(`./src/${item.slug}.html`, res, function (err) {
+          if (err) { throw err };
+          console.log(`Saved: /src/${item.slug}.html`);
+        });
+      }
+    });
+  });
+};
